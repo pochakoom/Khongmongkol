b55406ec1d32e1a0bfd40328158a1bd11e069c9f
Pochakoom_s <116154880+pochakoom@users.noreply.github.com>
2024-09-26 00:02:49 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:khongmongkol/screen/home_screen.dart';
+import 'package:khongmongkol/welcome_page.dart';
+import 'screen/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //home: const WelcomePage(),
+      initialRoute: "/",
+      routes: {
+        "/":(context) => const WelcomePage(),
+        "/login" : (context) => const LoginScreen(),
+        "/home" : (context) => const HomeScreen(),
+        },
+    );
+  }
+}
